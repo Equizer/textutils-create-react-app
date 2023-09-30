bc9d7d06b91205a8558d40e83f179a9874f673df
Equizer <130571773+Equizer@users.noreply.github.com>
2023-09-30 20:35:18 +0530

    add following fetaures: convert to lower case, text summary that displays number of words, number of characters, minute required to read text, preview

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -12,15 +12,30 @@ export default function TextForm(props) {
     setText(event.target.value);
   }
 
+  const hadleLowerCaseClick = () => {
+    const newText = text.toLowerCase()
+    setText(newText);
+  }
 
   return (
+    <>
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10" placeholder="Enter Here..."></textarea>
       </div>
       <button className="btn btn-primary mx-1 my-1" onClick={handleUpperCaseClick}>Convert to Upper Case</button>
+      <button className="btn btn-primary" onClick={hadleLowerCaseClick}>Convert To Lower Case</button>
     </div>
+    <div className="container">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(' ').length} words | {text.length} Characters</p>
+      <p>{((text.split(' ').length) / 238).toFixed(2)} Minutes to read</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
+    </div>
+    
+    </>
   );
 }
 
