7ee3d923e99287138e26bca9d305b8ceada6e25b
Equizer <130571773+Equizer@users.noreply.github.com>
2023-10-01 03:34:25 +0530

    make another feature to clear text and  avoid printing 1 when the text is ''

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -8,13 +8,27 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   }
-  const handleOnChange = (event) => {
-    setText(event.target.value);
-  }
 
   const hadleLowerCaseClick = () => {
     const newText = text.toLowerCase()
     setText(newText);
+  }
+
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  }
+
+  const clearText = () => {
+    setText('');
+  }
+
+
+  function countWords  () {
+    if (text === '') {
+      return(0);
+    }
+    return (text.split(' ').length);
   }
 
   return (
@@ -24,12 +38,13 @@ export default function TextForm(props) {
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10" placeholder="Enter Here..."></textarea>
       </div>
-      <button className="btn btn-primary mx-1 my-1" onClick={handleUpperCaseClick}>Convert to Upper Case</button>
-      <button className="btn btn-primary" onClick={hadleLowerCaseClick}>Convert To Lower Case</button>
+      <button className="btn btn-primary mx-1 mb-1" onClick={handleUpperCaseClick}>Convert to Upper Case</button>
+      <button className="btn btn-primary mx-1 mb-1" onClick={hadleLowerCaseClick}>Convert To Lower Case</button>
+      <button className="btn btn-danger mx-1 mb-1" onClick={clearText}>Clear Text</button>
     </div>
     <div className="container">
       <h1>Your Text Summary</h1>
-      <p>{text.split(' ').length} words | {text.length} Characters</p>
+      <p>{countWords()} words | {text.length} Characters | {}Without Spaces</p>
       <p>{((text.split(' ').length) / 238).toFixed(2)} Minutes to read</p>
       <h3>Preview</h3>
       <p>{text}</p>
